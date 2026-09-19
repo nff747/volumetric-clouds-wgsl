6382548448aebee73de1452891f1a6f20b0152f8
nff747 <nff747@users.noreply.github.com>
2026-09-19 20:10:00 -0300

    fix(exports): correct relative path exports for phase, noise, and shader modules

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
 /**
- * volumetric-clouds-wgsl
- * Real-Time WebGPU Volumetric Cloudscapes & Atmospheric Raymarching Engine in WGSL
- * (c) 2026 nff747. Released under the MIT License.
+ * Volumetric Clouds WGSL
+ * Real-Time WebGPU Volumetric Cloud & Atmospheric Raymarching Engine in WGSL
+ * @packageDocumentation
  */
 
 export * from './types';
-export * from './math/henyeyGreenstein';
-export * from './math/worleyNoise';
-export * from './core/cloudRenderer';
-export * from './core/cpuReferenceClouds';
-export * from './renderer/ThreeCloudPass';
+export * from './math/phase';
+export * from './math/noise';
+export * from './core/CloudRenderer';
+export * from './core/CPUReferenceClouds';
+export * from './core/ThreeCloudPass';
 export * from './utils/blueNoise';
 export * from './utils/weather';
-export { VOLUMETRIC_CLOUDS_WGSL } from './shaders/clouds.wgsl';
+
+export { cloudMarchShader } from './shaders/cloudMarch.wgsl';
